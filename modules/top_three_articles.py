591a36e43b4be100a21f7f5cdf3919a4a3494034
Emmanuel Robles <emmanuelrobles94@gmail.com>
2019-02-16 02:03:01 -0500

    started top three articles

--- modules/top_three_articles.py
+++ modules/top_three_articles.py
@@ -3,8 +3,12 @@ import connection
 
 def get_top_three_articles():
 
-    db_cursor = connection.Connection.get_connection()
+    conn = connection.Connection()
 
-    db_cursor.execute("SELECT * FROM articles")
+    db_cursor = conn.get_connection()
+
+    db_cursor.execute("SELECT path, count(*) as count FROM log where status = '200 OK' group by (path) order by count desc")
 
     print db_cursor.fetchall()
+
+get_top_three_articles()
