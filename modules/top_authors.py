8af5e9e780f185b7b85957aac4c5775a760285b4
Emmanuel Robles <emmanuelrobles94@gmail.com>
2019-02-16 20:24:13 -0500

    added get top authors

---
+++ modules/top_authors.py
@@ -0,0 +1,23 @@
+import connection
+
+
+def get_top_authors():
+
+    conn = connection.Connection()
+
+    db_cursor = conn.get_connection()
+
+    path_folder = "/article/"
+
+    querry = "SELECT name,count(*) " \
+             "FROM articles " \
+             "INNER JOIN log ON articles.slug = substring(log.path,length('/article/')+1) " \
+             "INNER JOIN authors on author = authors.id " \
+             "GROUP BY name " \
+             "ORDER BY count DESC;".format(path_folder)
+
+    db_cursor.execute(querry)
+
+    return db_cursor.fetchall()
+
+print get_top_authors()
